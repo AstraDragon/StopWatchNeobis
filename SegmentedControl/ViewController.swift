ed417b3ad7b527b757855e8a97d845d8ef0c47f6
Astra Moldakunova <astramalbikova@gmail.com>
2022-06-24 23:46:01 +0600

    Initial Commit

---
+++ SegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedControl
+//
+//  Created by Astra on 24/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
